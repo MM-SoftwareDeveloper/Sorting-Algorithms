5615ccfb4c945603ef88d17dd23c4a5f11675675
milan-dev <milan.varnamo@gmail.com>
2020-10-26 17:10:13 +0100

    Added code for selection sort

---
+++ src/Selection_Sort/Main.java
@@ -0,0 +1,14 @@
+package Selection_Sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {7,3,1,4,6,2,3};
+
+        SelectionSort sorter = new SelectionSort();
+
+        sorter.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
---
+++ src/Selection_Sort/SelectionSort.java
@@ -0,0 +1,30 @@
+package Selection_Sort;
+
+/**
+ * Selection Sort Class O(n^2)
+ */
+
+public class SelectionSort {
+
+    public void sort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            int minIndex = i;
+
+            for (int j = i; j < array.length; j++) {
+
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(array, minIndex, i);
+        }
+    }
+
+    private void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
